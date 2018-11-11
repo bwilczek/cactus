92b038bbca34c44e2cdf1c2972dbef8c41f8bcad
Bartek Wilczek <bartek.wilczek@toptal.com>
2018-11-11 09:43:01 +0100

    Introduce Router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import '../css/App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import SignUpPage from './SignUpPage'
+import SignInPage from './SignInPage'
+import HomePage from './HomePage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/sign_in" component={SignInPage} />
+          <Route path="/sign_up" component={SignUpPage} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
